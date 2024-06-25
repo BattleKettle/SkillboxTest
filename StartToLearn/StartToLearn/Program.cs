924f824c3be8bed9b2b7d68b24cb16ef2d8fa2b4
BattleKettle <nechet1999q@mail.ru>
2024-06-25 21:26:59 +0300

    1.4 Practice

---
+++ StartToLearn/StartToLearn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartToLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир! \nЯ снова сюда попал." );
+            Console.WriteLine("Это однострочный вывод");
+            Console.WriteLine("И это однострочный");
+            Console.WriteLine("А это раздельный\nвывод текста");
+            Console.Write("Hello");
+            Console.Write(" World");
+            Console.Write(" >:D");
+
+            //Console.ReadKey(); почему нельзя использовать эту задержку, так же тоже должно работать?
+           
+            Console.ReadLine();
+        }
+    }
+}
